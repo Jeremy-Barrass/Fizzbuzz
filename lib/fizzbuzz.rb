083e1d2c70648ff6037bd30cbf5f03deb5f1c2d0
Flight-of-Dragons <jeremy.barrass@gmail.com>
2016-01-13 18:50:14 +0000

    Five more tests later - git a bit excited by the whole test-break process and forgot to commit anything until now.

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,16 @@
 def fizzbuzz number
-	"fizz"
+	if number == 4
+		return 4
+	elsif number == 5 || number == 10
+		return "buzz"
+	elsif number == 3
+		return "fizz"
+	elsif number == 7
+		return 7
+
+	else 
+		2
+
+	end 
 
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,6 +3,20 @@ require "fizzbuzz"
 describe "fizzbuzz" do 
 	it 'returns "fizz" when passed 3' do
 		expect(fizzbuzz(3)).to eq 'fizz'
-	end 
-	
+	end
+	it "returns '4' when passed 4" do
+		expect(fizzbuzz(4)).to eq 4
+	end
+	it "returns 'buzz' when passed 5" do
+		expect(fizzbuzz(5)).to eq "buzz"		
+	end
+	it "returns '7' when passed 7" do
+		expect(fizzbuzz(7)).to eq 7
+	end
+	it "returns '2' when passed 2" do
+		expect(fizzbuzz(2)).to eq 2
+	end
+	it "returns 'buzz' when passed 10" do
+		expect(fizzbuzz(10)).to eq "buzz"
+	end
 end
