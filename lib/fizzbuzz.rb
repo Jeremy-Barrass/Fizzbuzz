3484018be0dc148505dec499e5be240fb4ce644a
Flight-of-Dragons <jeremy.barrass@gmail.com>
2016-01-16 14:36:48 +0000

    Final Fizzbuzz solution and Spec file.  TDD'd.

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,16 +1,19 @@
 def fizzbuzz number
-	if number == 4
-		return 4
-	elsif number == 5 || number == 10
-		return "buzz"
-	elsif number == 3
+	fb = ""
+	fb = fb + "fizz" if number % 3 == 0
+	fb = fb + "buzz" if number % 5 == 0
+	return number if fb.empty?
+	fb
+
+=begin	
+	if number % 3 == 0 && number % 5 == 0
+		return "fizzbuzz"
+	elsif number % 3 == 0
 		return "fizz"
-	elsif number == 7
-		return 7
-
+	elsif number % 5 == 0
+		return "buzz"
 	else 
-		2
-
+		return number	
 	end 
-
+=end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -19,4 +19,17 @@ describe "fizzbuzz" do
 	it "returns 'buzz' when passed 10" do
 		expect(fizzbuzz(10)).to eq "buzz"
 	end
+
+	it "returns 'fizzbuzz' when passed 15" do
+		expect(fizzbuzz(15)).to eq "fizzbuzz"
+	end
+	it "should return 'fizzbuzz' when passed 30" do
+		expect(fizzbuzz(30)).to eq "fizzbuzz"
+	end
+	it "should return 'fizz' when passed 18" do
+		expect(fizzbuzz(18)).to eq "fizz"
+	end
+	it "should return 'buzz' when passed 25" do
+		expect(fizzbuzz(25)).to eq "buzz"
+	end
 end
